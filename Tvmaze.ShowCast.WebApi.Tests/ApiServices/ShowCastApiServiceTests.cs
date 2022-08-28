c2b14eedc33e85c75df9f83b4ff5ccaa068d95eb
andrey-yagovkin <andrey.yagovkin@cloudmore.com>
2022-08-28 19:26:21 +0300

    Added WebApiTests

---
+++ Tvmaze.ShowCast.WebApi.Tests/ApiServices/ShowCastApiServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Moq;
+using Tvmaze.ShowCast.Core.Bll.Models;
+using Tvmaze.ShowCast.Core.Bll.Services;
+using Tvmaze.ShowCast.WebApi.ApiServices;
+
+namespace Tvmaze.ShowCast.WebApi.Tests.ApiServices;
+
+public class ShowCastApiServiceTests
+{
+    private readonly Mock<IShowCastService> _showCastService;
+    private readonly ShowCastApiService _sut;
+
+    public ShowCastApiServiceTests()
+    {
+        _showCastService = new Mock<IShowCastService>();
+        _sut = new ShowCastApiService(_showCastService.Object);
+    }
+    
+    [Fact]
+    private async Task GetSortedDescPage_SortingWorksFine()
+    {
+        // Arrange
+        var page = 1;
+
+        var castOld = new CastModel(1, "cast 1", new DateOnly(1950, 1, 1));
+        var castYounger = new CastModel(2, "cast 2", new DateOnly(1959, 1, 1));
+        var castYoungest = new CastModel(3, "cast 3", new DateOnly(1977, 1, 1));
+
+        var showCast = new List<ShowCastModel> 
+        {         
+            new (1, "show 1", new List<CastModel> { castOld, castYounger, castYoungest }),
+            new (2, "show 2", new List<CastModel> { castYoungest, castYounger, castOld }),
+            new (3, "show 3", new List<CastModel> { castOld, castYoungest, castYounger })
+        };
+            
+        // Expectations
+        _showCastService.Setup(x => x.GetPageAsync(page, CancellationToken.None)).ReturnsAsync(showCast);
+        
+        // Act
+        var actual = await _sut.GetSortedDescPage(page, CancellationToken.None);
+        
+        // Assert
+        actual.Select(x => x.Cast.Should().BeInDescendingOrder(x => x.BirthDay));
+        _showCastService.Verify(mock => mock.GetPageAsync(page, CancellationToken.None), Times.Once());
+    }
+}
